c5a7d7600f25c7bc9b7843ff87e97eba2db7757e
Dang3rK1LL <damjan.dani@gmail.com>
2023-11-21 11:11:18 +0100

    jsgyak + geek

---
+++ gyak9/wwwroot/geek.js
@@ -0,0 +1,25 @@
+﻿var viccek;
+
+function letöltés() {
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+}
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+    const jokesContainer = document.getElementById('jokes-container');
+    jokesContainer.innerHTML = '';
+    viccek.forEach(jokeObj => {
+        const jokeElement = document.createElement('div');
+        jokeElement.textContent = jokeObj.text;
+        jokesContainer.appendChild(jokeElement);
+    });
+}
+
+window.onload = function () {
+    letöltés();
+}
---
+++ gyak9/wwwroot/jsgyak.js
@@ -0,0 +1,28 @@
+﻿function createDivs(count) {
+    const divs = [];
+    for (let i = 0; i < count; i++) {
+        const div = document.createElement('div');
+        divs.push(div);
+    }
+    return divs;
+}
+
+function fillPascalsTriangle(divs) {
+    const rows = divs.length;
+    for (let row = 0; row < rows; row++) {
+        let number = 1;
+        for (let col = 0; col <= row; col++) {
+            divs[row].textContent += number + ' ';
+            number = number * (row - col) / (col + 1);
+        }
+    }
+}
+
+function displayDivs(containerId, divs) {
+    const container = document.getElementById(containerId);
+    divs.forEach(div => container.appendChild(div));
+}
+
+const divs = createDivs(10);
+fillPascalsTriangle(divs);
+displayDivs('pascal-triangle', divs);
